42ac04d37defc46c76b5493bfaffc363f21f6472
Astrator99 <100300535+Astrator99@users.noreply.github.com>
2024-06-25 13:11:27 +0200

    Workshop Day02
    - Widgeth
    - Game Mode
    - Game Mode Menu
    - FirstPerson
    - FirstPersonProjectile
    - FirstPersonCharacter

---
+++ Source/BouncyBall/MyClass.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class BOUNCYBALL_API MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+};
